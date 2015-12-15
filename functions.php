bd2ddf242dd95f564b493ddb4190aa7a60f7d894
Elvis Martins <elvisbmartins@gmail.com>
2015-12-14 22:14:25 -0200

    Adicionado Congelado-functions, adicionar post-type, metabox, actions

--- functions.php
+++ functions.php
@@ -150,3 +150,9 @@ require get_template_directory() . '/inc/customizer.php';
  * Load Jetpack compatibility file.
  */
 require get_template_directory() . '/inc/jetpack.php';
+
+
+/*
+* Includes Congelado: PostTypes, MetaBoxes, Actions,
+*/
+include dirname(__FILE__).'/includes/congelado-functions.php';
