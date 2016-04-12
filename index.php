f4a1352af830d7616852a7e4b99324bdff69dedd
Gustavo Gus <gusgustavo@users.noreply.github.com>
2016-04-12 20:25:07 +0000

    added destaquesecundario category #44

--- index.php
+++ index.php
@@ -94,7 +94,7 @@ get_header(); ?>
             $args = array(
                 'post_type' => 'post',
                 'posts_per_page' => 3,
-                'category_name' => 'noticias',
+                'category_name' => 'destaquesecundario',
                 );
 
             $loop_carrousel = new WP_Query($args);
@@ -135,7 +135,7 @@ get_header(); ?>
               $args = array(
                 'post_type' => 'post',
                 'posts_per_page' => 3,
-                'category_name' => 'noticias',
+                'category_name' => 'destaquesecundario',
                 );
 
               $loop_carrousel = new WP_Query($args);
