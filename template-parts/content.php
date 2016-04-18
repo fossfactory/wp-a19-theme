04d8be1c86352bd4fb5a5e52bff24d6d5a0c6784
Gustavo Gus <gusgustavo@users.noreply.github.com>
2016-04-18 19:54:35 +0000

    Update content.php

--- template-parts/content.php
+++ template-parts/content.php
@@ -41,7 +41,7 @@ $post_id = get_the_id();
             <div style="float: right; margin-top:7px;">
                <a href="whatsapp://send?text=<?php the_title();?> - <?php echo wp_get_shortlink();?>" data-action="share/whatsapp/share"><i class="fa fa-1x fa-fw fa-lg fa-whatsapp text-danger"></i></a>
                <a href="https://twitter.com/home/?status=<?php the_title();?> - <?php echo wp_get_shortlink();?>" title="Tweet!"><i class="fa fa-1x fa-fw fa-lg fa-twitter text-danger"></i></a>
-               <a href="https://www.facebook.com/sharer/sharer.php?u=<?php the_title();?>&t=<?php echo wp_get_shortlink();?>" title="Compartilhe no Facebook!"><i class="fa fa-1x fa-fw fa-lg fa-facebook text-danger"></i></a>
+               <a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo wp_get_shortlink();?>" title="Compartilhe no Facebook!"><i class="fa fa-1x fa-fw fa-lg fa-facebook text-danger"></i></a>
             </div>
             <br>
             <hr>
@@ -53,7 +53,7 @@ $post_id = get_the_id();
             <div style="float:right; margin-top:2px; margin-bottom:1px;">
                <a href="whatsapp://send?text=<?php the_title();?> - <?php echo wp_get_shortlink();?>" data-action="share/whatsapp/share"><i class="fa fa-1x fa-fw fa-lg fa-whatsapp text-danger"></i></a>
                <a href="https://twitter.com/home/?status=<?php the_title();?> - <?php echo wp_get_shortlink();?>" title="Twitter!"><i class="fa fa-1x fa-fw fa-lg fa-twitter text-danger"></i></a>
-               <a href="https://www.facebook.com/sharer/sharer.php?u=<?php the_title(); ?>&t=<?php echo wp_get_shortlink();?>" title="Compartilhe no Facebook!"><i class="fa fa-1x fa-fw fa-lg fa-facebook text-danger"></i></a>
+               <a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo wp_get_shortlink();?>" title="Compartilhe no Facebook!"><i class="fa fa-1x fa-fw fa-lg fa-facebook text-danger"></i></a>
          
             </div>
             <div style:"float:left; margin-top:9px; margin-bottom:2px;"><b>LINK: </b><?php echo wp_get_shortlink(); ?></div>
