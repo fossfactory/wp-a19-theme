8ad825e2a88ea8eea4b206f2de09c517b17d5c1f
Gustavo Gus <gusgustavo@users.noreply.github.com>
2016-04-18 21:21:52 +0000

    Update content.php

--- template-parts/content.php
+++ template-parts/content.php
@@ -47,13 +47,7 @@ $post_id = get_the_id();
             <hr>
             </hr>
            		<div class="materia-conteudo">
-           		<?php
-				$img_src = wp_get_attachment_image_url( $attachment_id, 'medium' );
-				$img_srcset = wp_get_attachment_image_srcset( $attachment_id, 'medium' );
-			?>
-			<img src="<?php echo esc_url( $img_src ); ?>"
-     			srcset="<?php echo esc_attr( $img_srcset ); ?>"
-     			sizes="(max-width: 50em) 87vw, 680px" alt="">
+           		<div class="img-float"><?php the_post_thumbnail( 'thumbnail', array( 'class' => 'img-responsive' ) ); ?></div>
            		<?php the_content( ); ?>
            		</div>
             <hr>
