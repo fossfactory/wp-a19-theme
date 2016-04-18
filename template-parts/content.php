0612070ed75874a7e3c72469ab1ba5e5e27662e5
Gustavo Gus <gusgustavo@users.noreply.github.com>
2016-04-18 20:31:05 +0000

    Update content.php

--- template-parts/content.php
+++ template-parts/content.php
@@ -204,7 +204,7 @@ $post_id = get_the_id();
               $category_link = get_category_link( $category_id );
           ?>
          <li class="media a-orange">
-          <div align="center"><h4><a href="<?php echo esc_url( $category_link ); ?>" title="noticias" class="text-orange">Veja mais</a></h4></div>
+          <div align="center"><h4><a href="<?php echo esc_url( $category_link ); ?>" title="noticias" class="text-orange"><img src="<?php bloginfo('template_url'); ?>/images/mais.jpg"> Veja mais</a></h4></div>
          </li>
 
          </ul>
@@ -246,7 +246,7 @@ $post_id = get_the_id();
               $category_link = get_category_link( $category_id );
           ?>
           <li class="media a-orange">
-          <div align="center"><h4><a href="<?php echo esc_url( $category_link ); ?>" title="publicacoes" class="text-orange"><img src="<?php bloginfo('template_url'); ?>/images/mais.jpg">Veja mais</a></h4></div>
+          <div align="center"><h4><a href="<?php echo esc_url( $category_link ); ?>" title="publicacoes" class="text-orange"><img src="<?php bloginfo('template_url'); ?>/images/mais.jpg"> Veja mais</a></h4></div>
          </li>
             </ul>
             <hr>
