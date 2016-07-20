f15dd227de5069d0651d5a3080c8c243a3138f3a
quijaua <folhas@quijaua.com.br>
2016-07-20 11:34:25 -0300

    Imagem responsiva no corpo do post

--- functions.php
+++ functions.php
@@ -374,3 +374,20 @@ function pagination($pages = '', $range = 4)
          echo "</div>\n";
      }
 }
+add_filter('wp_npagination','pagination');
+
+// Override img caption shortcode to fix 10px issue.
+add_filter('img_caption_shortcode', 'fix_img_caption_shortcode', 10, 3);
+
+function fix_img_caption_shortcode($val, $attr, $content = null) {
+    extract(shortcode_atts(array(
+        'id'    => '',
+        'align' => '',
+        'width' => '',
+        'caption' => ''
+    ), $attr));
+
+    if ( 1 > (int) $width || empty($caption) ) return $val;
+
+    return '<div id="' . $id . '" class="wp-caption ' . esc_attr($align) . '" style="max-width: 90%;margin-left:-15px">' . do_shortcode( $content ) . '<p class="wp-caption-text">' . $caption . '</p></div>';
+}
